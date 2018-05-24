4ef5f54a5031787766ddfae6211183979f153729
Erica Droz <ericad2891@gmail.com>
2018-05-24 00:27:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,7 +4,7 @@ function updatePlaylist(playlist, Kiiara, Feels){
   playlist[Kiiara]=Feels
   return playlist
 }
-function removeFromPlaylist(){
+function removeFromPlaylist(playlist, Kiiara){
   delete playlist.Kiiara
   return playlist
 }
